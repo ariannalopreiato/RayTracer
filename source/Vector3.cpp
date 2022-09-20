02983644720405d0a9c00022066861b160200ba5
ariannalopreiato <ariannalopreiato@hotmail.com>
2022-09-20 14:37:47 +0200

    Dot and Cross product

--- source/Vector3.cpp
+++ source/Vector3.cpp
@@ -45,16 +45,18 @@ namespace dae {
 
 	float Vector3::Dot(const Vector3& v1, const Vector3& v2)
 	{
-		//todo W1
-		assert(false && "Not Implemented Yet");
-		return {};
+		float dot = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+		//assert(false && "Not Implemented Yet");
+		return dot;
 	}
 
 	Vector3 Vector3::Cross(const Vector3& v1, const Vector3& v2)
 	{
-		//todo W1
-		assert(false && "Not Implemented Yet");
-		return {};
+		float x = v1.y * v2.z - v1.z * v2.y;
+		float y = v1.z * v2.x - v1.x * v2.z;
+		float z = v1.x * v2.y - v1.y * v2.x;
+		//assert(false && "Not Implemented Yet");
+		return Vector3{ x,y,z };
 	}
 
 	Vector3 Vector3::Project(const Vector3& v1, const Vector3& v2)
